c628088aa96f5abbed43ead2a8a3cc7d948113ae
skysugar <iskysugar@gmail.com>
2021-09-24 16:49:06 +0800

    update something

--- i3/lockscreen.sh
+++ i3/lockscreen.sh
@@ -34,12 +34,12 @@ locktext="主人不在,大门敞开!"
 
 i3lock \
 	-c 000000 \
-	--timepos="x+110:h-70" \
-	--clock --date-align 1 --datestr "$locktext" \
-	--insidecolor=$background --ringcolor=$foreground --line-uses-inside \
-	--keyhlcolor=$letterEnteredColor --bshlcolor=$letterRemovedColor --separatorcolor=$background \
-	--insidevercolor=$passwordCorrect --insidewrongcolor=$passwordIncorrect \
-	--ringvercolor=$foreground --ringwrongcolor=$foreground \
-	--radius=20 --ring-width=4 --veriftext="" --wrongtext="" \
-	--verifcolor="$foreground" --timecolor="$foreground" --datecolor="$foreground" \
-	--noinputtext="" --force-clock $lockargs
+	--ind-pos="x+110:h-70" \
+	--clock --date-align 1 --date-str "$locktext" \
+	--inside-color=$background --ring-color=$foreground --line-uses-inside \
+	--keyhl-color=$letterEnteredColor --bshl-color=$letterRemovedColor --separator-color=$background \
+	--insidever-color=$passwordCorrect --insidewrong-color=$passwordIncorrect \
+	--ringver-color=$foreground --ringwrong-color=$foreground \
+	--radius=20 --ring-width=4 --verif-text="" --wrong-text="" \
+	--verif-color="$foreground" --time-color="$foreground" --date-color="$foreground" \
+	--noinput-text="" --force-clock $lockargs
